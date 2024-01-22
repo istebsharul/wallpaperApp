f2b2ce3ab7709bf9dcfc5bad0b785cbce453405a
istebsharul <istebsharulbari919@gmail.com>
2024-01-22 07:42:30 +0530

    Download and back button added,Set wallpaper is yet to fixed.

--- lib/controller/ApiOpr.dart
+++ lib/controller/ApiOpr.dart
@@ -4,60 +4,44 @@ import 'package:http/http.dart' as http;
 import 'package:wallpaper/model/photosModel.dart';
 import 'package:wallpaper/model/categoryModel.dart';
 
-
-class ApiOperations{
-  static List<PhotosModel> trendingWallpapers=[];
-  static Future<List<PhotosModel>> getTrendingWallpaper() async{
-    await http.get(
-      Uri.parse("https://api.pexels.com/v1/curated"),
-      headers: {"Authorization": "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"}
-    ).then((value) {
+class ApiOperations {
+  static List<PhotosModel> trendingWallpapers = [];
+  static Future<List<PhotosModel>> getTrendingWallpaper() async {
+    await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
+      "Authorization":
+          "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"
+    }).then((value) {
       Map<String, dynamic> jsonData = jsonDecode(value.body);
-        List photos = jsonData['photos'];
-        jsonData['photos'].forEach((element){
-            trendingWallpapers.add(PhotosModel.fromApiToApp(element));
-            // Map<String, dynamic> src = element["src"];
-            // print(src["portrait"]);
-        });
+      List photos = jsonData['photos'];
+      jsonData['photos'].forEach((element) {
+        trendingWallpapers.add(PhotosModel.fromApiToApp(element));
+      });
     });
     return trendingWallpapers;
-  } 
-  
-  static List<PhotosModel> searchWallpapersList=[];
-  static Future<List<PhotosModel>> getSearchWallpapers(String query) async{
+  }
+
+  static List<PhotosModel> searchWallpapersList = [];
+  static Future<List<PhotosModel>> getSearchWallpapers(String query) async {
     await http.get(
-      Uri.parse("https://api.pexels.com/v1/search?query=$query&per_page=30&page=1"),
-      headers: {"Authorization": "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"}
-    ).then((value) {
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=30&page=1"),
+        headers: {
+          "Authorization":
+              "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"
+        }).then((value) {
       Map<String, dynamic> jsonData = jsonDecode(value.body);
-        List photos = jsonData['photos'];
-        jsonData['photos'].forEach((element){
-            searchWallpapersList.clear();
-            photos.forEach((element) { 
-              searchWallpapersList.add(PhotosModel.fromApiToApp(element));
-            });
-            
+      List photos = jsonData['photos'];
+      jsonData['photos'].forEach((element) {
+        searchWallpapersList.clear();
+        photos.forEach((element) {
+          searchWallpapersList.add(PhotosModel.fromApiToApp(element));
         });
+      });
     });
     return searchWallpapersList;
   }
 
   static List<CategoryModel> cateogryModelList = [];
-  // static List<categoryModel> getCategoryList(){
-  //   List categoryName=[
-  //     "Cars","Nature","Bikes","Street","City","Flowers"
-  //   ];
-  //   categoryModelList.clear();
-  //   cateogryName.forEach((catName) async{
-  //     final _random = new Random();
-
-  //     PhotosModel photoModel= (await getSearchWallpapers(catName))[0 + _random.nextInt(11-0)];
-  //     // print("")
-  //     print(photoModel.imgSrc);
-  //     cateogryModelList.add(categoryModel(catImgUrl: photoModel.imgSrc,catName:catName));
-  //   }
-  //   );
-  // }
 
   static List<CategoryModel> getCategoriesList() {
     List cateogryName = [
--- lib/views/screens/fullScreen.dart
+++ lib/views/screens/fullScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
+import 'package:open_file/open_file.dart';
 
 import '../widgets/customAppBar.dart';
 
@@ -6,55 +11,117 @@ class FullScreen extends StatelessWidget {
   String imgUrl;
   FullScreen({super.key, required this.imgUrl});
 
+  Future<void> setWallpaper(String url) async {
+    try {
+      int location = WallpaperManager
+          .BOTH_SCREEN; // or location = WallpaperManager.LOCK_SCREEN;
+      var file = await DefaultCacheManager().getSingleFile(url);
+      final bool result =
+      await WallpaperManager.setWallpaperFromFile(file.path, location);
+      print(result);
+    } on PlatformException {}
+  }
+
+  Future<void> downloadWallpaper(
+      String wallpaperUrl, BuildContext context) async {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Downloading Started...")));
+    try {
+      // Saved with this method.
+      var imageId = await ImageDownloader.downloadImage(wallpaperUrl);
+      if (imageId == null) {
+        return;
+      }
+      // Below is a method of obtaining saved image information.
+      var fileName = await ImageDownloader.findName(imageId);
+      var path = await ImageDownloader.findPath(imageId);
+      var size = await ImageDownloader.findByteSize(imageId);
+      var mimeType = await ImageDownloader.findMimeType(imageId);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Downloaded Sucessfully"),
+        action: SnackBarAction(
+            label: "Open",
+            onPressed: () {
+              OpenFile.open(path);
+            }),
+      ));
+      print("IMAGE DOWNLOADED");
+    } on PlatformException catch (error) {
+      print(error);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Error Occured - $error")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () async {
-          // Handle back button press event here
-          Navigator.pop(context); // Navigate to previous screen
-          return false; // Return true to allow back button press, false to prevent it
-        },
-        child: Scaffold(
-            body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(imgUrl),
-              fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(imgUrl),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        )));
+          Positioned(
+            top: 30.0,
+            left: 16.0,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context); // Navigate to previous screen
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      await downloadWallpaper(imgUrl, context);
+                      print("Second button pressed");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white.withOpacity(0.5),
+                      onPrimary: Colors.white,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: Text('Download Now'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      // await downloadWallpaper(imgUrl, context);
+                      await setWallpaper(imgUrl);
+                      print("Second button pressed");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white.withOpacity(0.5),
+                      onPrimary: Colors.white,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: Text('Set Wallpaper'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-
-    // return Scaffold(
-      //  appBar: AppBar(
-      //     backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      //     centerTitle: true,
-      //     elevation: 0.0,
-      //     // title: customAppBar(
-      //     //   word1: "Wallpaper",
-      //     //   word2: "Shar",
-      //     // ),
-      //     leading: IconButton(
-      //       icon: Icon(Icons.arrow_back),
-      //       color:Colors.grey,
-      //       onPressed: () {
-      //         Navigator.pop(context);
-      //       },
-      //     ),
-      //   ),
-//       body: Container(
-//         height: MediaQuery.of(context).size.height,
-//         width: MediaQuery.of(context).size.width,
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//             image: NetworkImage(imgUrl),
-//             fit:BoxFit.cover, 
-//           )
-//         ),
-//       ),
-//     );
-//   }
-// }
