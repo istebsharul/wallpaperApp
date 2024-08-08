04f19224dcf331fc21999a740af7a156e46017a6
Md Istebsharul bari <70230551+istebsharul@users.noreply.github.com>
2024-08-08 17:32:05 +0530

    Update ApiOpr.dart
    Caching Implemented

--- lib/controller/ApiOpr.dart
+++ lib/controller/ApiOpr.dart
@@ -67,3 +67,74 @@ class ApiOperations {
     return cateogryModelList;
   }
 }
+
+
+// class ApiOperations {
+//   static List<PhotosModel> trendingWallpapers = [];
+
+//   static Future<List<PhotosModel>> getTrendingWallpaper() async {
+//     // Check if data is already cached
+//     if (trendingWallpapers.isNotEmpty) {
+//       return trendingWallpapers;
+//     }
+
+//     // Fetch data if not cached
+//     final response = await http.get(Uri.parse("https://api.pexels.com/v1/curated"), headers: {
+//       "Authorization": "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"
+//     });
+
+//     if (response.statusCode == 200) {
+//       Map<String, dynamic> jsonData = jsonDecode(response.body);
+//       List photos = jsonData['photos'];
+//       photos.forEach((element) {
+//         trendingWallpapers.add(PhotosModel.fromApiToApp(element));
+//       });
+//     }
+
+//     return trendingWallpapers;
+//   }
+
+//   static List<PhotosModel> searchWallpapersList = [];
+
+//   static Future<List<PhotosModel>> getSearchWallpapers(String query) async {
+//     // Clear previous search results to avoid mixing results from different queries
+//     searchWallpapersList.clear();
+
+//     final response = await http.get(
+//         Uri.parse("https://api.pexels.com/v1/search?query=$query&per_page=30&page=1"),
+//         headers: {
+//           "Authorization": "BwcSEoqXqsT0x3Qn6X8jiELWGWzU2qQH6JBxTaNA2CRBw5XqcdJdz5jq"
+//         });
+
+//     if (response.statusCode == 200) {
+//       Map<String, dynamic> jsonData = jsonDecode(response.body);
+//       List photos = jsonData['photos'];
+//       photos.forEach((element) {
+//         searchWallpapersList.add(PhotosModel.fromApiToApp(element));
+//       });
+//     }
+
+//     return searchWallpapersList;
+//   }
+
+//   static List<CategoryModel> cateogryModelList = [];
+
+//   static Future<List<CategoryModel>> getCategoriesList() async {
+//     if (cateogryModelList.isNotEmpty) {
+//       return cateogryModelList;
+//     }
+
+//     List cateogryName = ["Cars", "Nature", "Bikes", "Street", "City", "Flowers"];
+//     cateogryModelList.clear();
+//     for (String catName in cateogryName) {
+//       final _random = new Random();
+//       List<PhotosModel> photos = await getSearchWallpapers(catName);
+//       if (photos.isNotEmpty) {
+//         PhotosModel photoModel = photos[_random.nextInt(photos.length)];
+//         cateogryModelList.add(CategoryModel(catImgUrl: photoModel.imgSrc, catName: catName));
+//       }
+//     }
+
+//     return cateogryModelList;
+//   }
+// }
